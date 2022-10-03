ecf92278e3538333e6ce9460d1b0660f3f0487b3
huzaifa3165 <83898816+huzaifa3165@users.noreply.github.com>
2022-10-03 20:45:32 +0500

    Updates

---
+++ precache-manifest.3f08f7a7372bc5a59b12b8ee7d514747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b943ddeb45b257c16bd3",
+    "url": "/monsters-rolodex/static/css/main.c403ad10.chunk.css"
+  },
+  {
+    "revision": "b943ddeb45b257c16bd3",
+    "url": "/monsters-rolodex/static/js/main.b943ddeb.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/monsters-rolodex/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "6a9ed8bb838c5d13005f",
+    "url": "/monsters-rolodex/static/js/2.6a9ed8bb.chunk.js"
+  },
+  {
+    "revision": "02c1b23ffcb7e58431d5",
+    "url": "/monsters-rolodex/static/js/runtime~main.02c1b23f.js"
+  },
+  {
+    "revision": "6a455cb552681583c27a89c44e53471d",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
